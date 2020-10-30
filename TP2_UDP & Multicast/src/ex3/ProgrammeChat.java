72a9d2efe66dd887a42cfd10c97fa1907a39792e
Gatari Jean Debout <jeandebout.gatari.etu@univ-lille.fr>
2020-10-30 23:55:22 +0100

    Programme qui Réunit les deux fonctions Multicasts afin de générer une conversation

---
+++ TP2_UDP & Multicast/src/ex3/ProgrammeChat.java
@@ -0,0 +1,20 @@
+package ex3;
+
+import java.io.IOException;
+import java.lang.Thread;
+
+/**
+ * A simple UDP multithreadead chat
+ * @author Thomas Liard, Jean Debout Gatari
+ *
+ */
+public class ProgrammeChat{
+	
+	public static void main(String[] args){
+		
+		/**
+		 *  À FAIREEEE
+		 */
+		
+	}
+}
